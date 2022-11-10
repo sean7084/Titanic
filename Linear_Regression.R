514ddabd87ba61f10a1d334f3c4d5a2d5674bb9f
Chang Liu <sean_cc@icloud.com>
2022-11-10 11:24:45 -0700

    Initial commit

--- Linear_Regression.R
+++ Linear_Regression.R
@@ -12,8 +12,8 @@ test$survival_rate = train_lm_alt$coefficients['(Intercept)']+
                      test$Age * train_lm_alt$coefficients['Age'] +  
                      test$SibSp * train_lm_alt$coefficients['SibSp']
 test$Survived = ifelse(test$survival_rate>.5,1,0)
-test$Survived = ifelse(is.na(test$survival_rate) == T,rbinom(n=1,size=1,prob=.5),test$Survived)
 
-select(test,c(PassengerId,Survived))
+summary(train$Survived)
+test$Survived = ifelse(is.na(test$survival_rate) == T,rbinom(n=1,size=1,prob=.3838),test$Survived)
 
 write.csv(select(test,c(PassengerId,Survived)),'~/Dropbox/R/Titanic/test_lm.csv',row.names = F)
