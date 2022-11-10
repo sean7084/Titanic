b03aa2ed9823e6543dac314c0e35040c6900de51
Chang Liu <sean_cc@icloud.com>
2022-11-10 13:54:31 -0700

    Initial commit

--- Linear_Regression.R
+++ Linear_Regression.R
@@ -1,11 +1,15 @@
+# Library
 library(tidyverse)
 
+# Initial linear model
 train_lm = lm(Survived~.,data = select(train,-c(Name,Ticket,Cabin)))
 summary(train_lm)
 
+# Linear model after removing non-significant IVs
 train_lm_alt = lm(Survived~.,data = select(train,c(Survived,Pclass,Sex,Age,SibSp)))
 summary(train_lm_alt)
 
+# Prediction using linear regression results
 test$survival_rate = train_lm_alt$coefficients['(Intercept)']+
                      test$Pclass * train_lm_alt$coefficients['Pclass'] +
                      ifelse(test$Sex == 'male',train_lm_alt$coefficients['Pclass'],0) +
@@ -13,7 +17,9 @@ test$survival_rate = train_lm_alt$coefficients['(Intercept)']+
                      test$SibSp * train_lm_alt$coefficients['SibSp']
 test$Survived = ifelse(test$survival_rate>.5,1,0)
 
+# Replace NAs with random binomial values with p = Overall survival rate
 summary(train$Survived)
 test$Survived = ifelse(is.na(test$survival_rate) == T,rbinom(n=1,size=1,prob=.3838),test$Survived)
 
+# Output the result into .csv file for submission
 write.csv(select(test,c(PassengerId,Survived)),'~/Dropbox/R/Titanic/test_lm.csv',row.names = F)
--- data_reformat.R
+++ data_reformat.R
@@ -1,17 +1,22 @@
-setwd('~/Dropbox/R/Titanic_temp')
+setwd('~/Dropbox/R/Titanic')
 
-# Libraries used for tidy up data
+# Libraries
 library(tidyverse)
+library(ggpubr)
 
 # Imprt data
 train = read.csv('train.csv')
 test = read.csv('test.csv')
+
+# Summary
 head(train)
 summary(train)
 
 # List cateogorical data into factors.
+as.factor
 
 # Check normaility of numerical data
+
 
 # Create a column for last name
 
