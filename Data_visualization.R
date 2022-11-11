64992273a34b7938a1559dbdac4a5d514fe9eb92
Chang Liu <sean_cc@icloud.com>
2022-11-10 20:37:15 -0700

    Initial commit

--- Data_visualization.R
+++ Data_visualization.R
@@ -2,3 +2,7 @@
 library(tidyverse)
 library(ggplot2)
 
+train$Survived = as.character(train$Survived)
+ggplot(train) +
+  geom_bar(mapping = aes(Sex, fill=Survived))
+
--- data_reformat.R
+++ data_reformat.R
@@ -29,5 +29,21 @@ shapiro.test(train$Fare)
 train$LastName = gsub("^(.*?),.*", "\\1", train$Name)
 #str_extract(train$Name,"(\\w+)") cannot extract first name like O'Really
 
+
+train$agesexgroup = rep(NA,nrow(train))
+# Create age & sex groups: young men, young women, married men, married women
+# Inspired by WCG model
+for (i in 1:nrow(train)){
+  if ('Mr' %in% train$Name[i]){
+    train$agesexgroup[i] = 'married man'
+  } else if ('Mrs'  %in% train$Name[i]){
+    train$agesexgroup[i] = 'married woman'
+  } else if ('Miss'  %in% train$Name[i]){
+    train$agesexgroup[i] = 'young woman'
+  } else if ('Master'  %in% train$Name[i]){
+    train$agesexgroup[i] = 'young man'
+  }
+}
+
 # Create cabin area column
 train$cabin_code = substr(train$Cabin,1,1)
