b99026867eedfcb88887526523452c66a7fd8e20
Chang Liu <sean_cc@icloud.com>
2022-11-10 21:11:46 -0700

    Initial commit

--- Data_visualization.R
+++ Data_visualization.R
@@ -2,7 +2,10 @@
 library(tidyverse)
 library(ggplot2)
 
-train$Survived = as.character(train$Survived)
+train$Survived = as.factor(train$Survived)
 ggplot(train) +
   geom_bar(mapping = aes(Sex, fill=Survived))
-
+ggplot(train, na.rm=T) +
+  geom_bar(mapping = aes(cabin_code, fill=Survived))
+ggplot(train, na.rm=T) +
+  geom_bar(mapping = aes(agesexgroup, fill=Survived))
---
+++ WCG.R
@@ -0,0 +1,47 @@
+# Create age & sex groups: young men, young women, married men, married women
+# Inspired by WCG model
+
+train$agesexgroup = rep(NA,nrow(train))
+
+for (i in 1:nrow(train)){
+  if (grepl('Mr',train$Name[i])){
+    train$agesexgroup[i] = 'married man'
+  } else if (grepl('Miss',train$Name[i])){
+    train$agesexgroup[i] = 'young woman'
+  } else if (grepl('Master',train$Name[i])){
+    train$agesexgroup[i] = 'young man'
+  } else if (grepl('Rev',train$Name[i])){
+    train$agesexgroup[i] = 'priest'
+  }
+  if (grepl('Mrs',train$Name[i])){
+    train$agesexgroup[i] = 'married woman'
+  }
+}
+
+train$agesexgroup = as.factor(train$agesexgroup)
+train %>% group_by(Survived,agesexgroup) %>% summarize(n=n()) 
+
+ggplot(train, na.rm=T) +
+  geom_bar(mapping = aes(agesexgroup, fill=Survived))
+
+# As we can see young people and married woman have a higher survival rate
+
+# Attempt 1: Assign women and young men as survived and others as not survived
+for (i in 1:nrow(test)){
+  if (grepl('Mr',test$Name[i])){
+    test$Survived[i] = 0 
+  } else if (grepl('Miss',test$Name[i])){
+    test$Survived[i] = 1
+  } else if (grepl('Master',test$Name[i])){
+    test$Survived[i] = 1
+  } else if (grepl('Rev',test$Name[i])){
+    test$Survived[i] = 0 
+  } else{
+    test$Survived[i] = rbinom(1,1,.5)
+  }
+  if (grepl('Mrs',test$Name[i])){
+    test$Survived[i] = 0 
+  }
+}
+# Output the result into .csv file for submission
+write.csv(select(test,c(PassengerId,Survived)),'~/Dropbox/R/Titanic/test_wcg_1.csv',row.names = F)
